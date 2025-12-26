7eb112b894baa713a70708adb6a5bf748470e215
richard <TU_USUARIO@users.noreply.github.com>
2025-12-26 18:25:22 -0400

    Agrega validación de correo electrónico y mejora la interacción del formulario de suscripción

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 const btn = document.querySelector('#btnSuscribe')
+const emailInput = document.querySelector('#email')
+const errorEmail = document.querySelector('.error__message')
+const suscribeContainer = document.querySelector('.container__suscribe')
+const article = document.querySelector('.article')
+const main = document.querySelector('.main')
+const btnDismiss = document.querySelector('#dismiss-button')
 
-btn.addEventListener('submit',(event) => {
+function validateEmail(email) { 
+  const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+  return re.test(String(email).toLowerCase())
+}
+
+btn.addEventListener('click',(event) => {
   event.preventDefault()
-  
-  console.log('1')
+  if (validateEmail(emailInput.value) === false) {
+    errorEmail.style.display = 'initial'
+    emailInput.classList.add('input__email--error')
+    return
+  } else {
+    errorEmail.style.display = 'none'
+    emailInput.classList.remove('input__email--error')
+    suscribeContainer.classList.add('is-visible')
+    main.classList.add('is-hidden')
+  }
+})
+
+btnDismiss.addEventListener('click', () => {
+  suscribeContainer.classList.remove('is-visible')
+  main.classList.remove('is-hidden')
+  emailInput.value = ''
 })
